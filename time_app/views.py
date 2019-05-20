f8efd4477629978ef8b81fe412d864acb2e67700
mthwbrwn <mthwbrwn@gmail.com>
2019-05-20 15:40:48 -0700

    added content to base and created home page

--- time_app/views.py
+++ time_app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+dummy = [
+    {
+        'client': 'wenatchee',
+        'project': 'dam building',
+        'time': 60,
+        'notes': 'first posting',
+        'date': 'now 2020',
+    },
+    {
+        'client': 'bellevue',
+        'project': 'city sewer',
+        'time': 55,
+        'notes': 'first posting for bellevue',
+        'date': 'then 2020',
+    }
+]
+
+
 def home(request):
-    return render(request, 'time_app/base.html')  
+    context = {
+        'dummy': dummy
+    }
+    return render(request, 'time_app/home.html', context)  
