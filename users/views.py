5784c3cf551c417194620b6bee3a1079f70d0a00
mthwbrwn <mthwbrwn@gmail.com>
2019-05-21 13:53:26 -0700

    added registration form -

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    '''
+    '''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Welcome back,!')
+        else:
+            form = UserCreationForm() 
+        return redirect('time-home')
+    return render(request, 'users/sign_in.html', {'form': form})
+
--- varius_project/settings.py
+++ varius_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'time_app.apps.TimeAppConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
