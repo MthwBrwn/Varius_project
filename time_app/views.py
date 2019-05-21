8f08ae1a1fb485f8695c199eec8a0bacd8b49c05
mthwbrwn <mthwbrwn@gmail.com>
2019-05-20 20:29:56 -0700

    added to models and home page

--- time_app/views.py
+++ time_app/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-dummy = [
-    {
-        'client': 'wenatchee',
-        'project': 'dam building',
-        'time': 60,
-        'notes': 'first posting',
-        'date': 'now 2020',
-    },
-    {
-        'client': 'bellevue',
-        'project': 'city sewer',
-        'time': 55,
-        'notes': 'first posting for bellevue',
-        'date': 'then 2020',
-    }
-]
+from .models import TimePost, Client
+from django.contrib.auth.models import User
 
 
 def home(request):
     context = {
-        'dummy': dummy
+        'user': User.objects.all(),
+        'posts': TimePost.objects.all(),
     }
     return render(request, 'time_app/home.html', context)  
