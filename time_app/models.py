7f89106cb57f1bc5a9d97a24b828fad4469e1c9e
mthwbrwn <mthwbrwn@gmail.com>
2019-05-20 22:44:37 -0700

    Revert "added to models and home page"
    This reverts commit 8f08ae1a1fb485f8695c199eec8a0bacd8b49c05.

--- time_app/models.py
+++ time_app/models.py
@@ -1,23 +1,3 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
-
-class TimePost(models.Model):
-    '''
-    '''
-    time_spent = models.FloatField()
-    notes = models.TextField()
-    date = models.DateField(default=timezone.now)
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.notes
-
-
-class Client(models.Model):
-    '''
-    '''
-    client = models.CharField(primary_key=True, max_length=255)
-    project_name = models.CharField(max_length=255)
+# Create your models here.
--- time_app/views.py
+++ time_app/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import TimePost, Client
-from django.contrib.auth.models import User
+
+dummy = [
+    {
+        'client': 'wenatchee',
+        'project': 'dam building',
+        'time': 60,
+        'notes': 'first posting',
+        'date': 'now 2020',
+    },
+    {
+        'client': 'bellevue',
+        'project': 'city sewer',
+        'time': 55,
+        'notes': 'first posting for bellevue',
+        'date': 'then 2020',
+    }
+]
 
 
 def home(request):
     context = {
-        'user': User.objects.all(),
-        'posts': TimePost.objects.all(),
+        'dummy': dummy
     }
     return render(request, 'time_app/home.html', context)  
