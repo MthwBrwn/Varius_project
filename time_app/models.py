d6eb6f96e6e8c21167549cbe8e4bf673279c142e
mthwbrwn <mthwbrwn@gmail.com>
2020-02-22 08:32:37 -0800

    changes made to allow projects to be archived out of regular user view

--- time_app/models.py
+++ time_app/models.py
@@ -22,7 +22,7 @@ class Project(models.Model):
     '''
     name = models.CharField(max_length=255)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
-    
+    archived = models.BooleanField(default=False, )
 
     def __str__(self):
         return self.name
